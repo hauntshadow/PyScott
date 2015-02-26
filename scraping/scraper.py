016f2a76c9a689e851a9506c0981f4834b934b70
Scott <Owner@Laptop.local>
2015-02-26 14:54:53 -0500

    Grabbed the title as well

--- scraping/scraper.py
+++ scraping/scraper.py
@@ -2,7 +2,7 @@ import urllib2
 from bs4 import BeautifulSoup
 
 '''Opens the selected URL and stores the html in the html variable'''
-req = urllib2.Request('http://www.imdb.com/title/tt0114709/?ref_=chttp_tt_102')
+req = urllib2.Request('http://www.imdb.com/title/tt0114709')
 response = urllib2.urlopen(req)
 html = (response.read())
 
@@ -14,11 +14,14 @@ html = (response.read())
 '''Creates a BeautifulSoup object on the html variable and stores it in soup'''
 soup = BeautifulSoup(html)
 '''Just prints the page to the console'''
-print soup.prettify()
+#print soup.prettify()
 
-''''Searches the soup variable for the tag titlePageSprite star-box-giga-star''''
-result = soup.find_all("div", "titlePageSprite star-box-giga-star")[0]
+''''Searches the soup variable for the tag titlePageSprite star-box-giga-star'''
+rating = soup.find_all("div", "titlePageSprite star-box-giga-star")[0]
+title = soup.find_all("span","itemprop")[0]
 
-'''Prints the full result and then prints just the contents which is the rating '''
-print str(result)
-print(result.renderContents())
+#print(str(title))
+#print str(rating)
+'''prints the title and rating'''
+print(title.renderContents())
+print(rating.renderContents())
